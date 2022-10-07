2fab7ca9eeed5ccb270eff3baecb80a8ec217bdb
Catalin-Mitu-isa <catalin.mitu@isa.utm.md>
2022-10-07 11:35:20 +0300

    Working sender with grpc

--- Sender/RPCSender.cpp
+++ Sender/RPCSender.cpp
@@ -1,13 +1,29 @@
 #include "RPCSender.h"
 
+RPCSender::RPCSender()
+{
+    std::stringstream channelAddress;
+    channelAddress << BROKER_NAME << ':' << BROKER_GRPC_PORT;
+    auto channel = grpc::CreateChannel(channelAddress.str(), grpc::InsecureChannelCredentials());
+    m_stub = SenderService::NewStub(std::move(channel));
+}
+
 bool RPCSender::createTopic(const std::string topic)
 {
-
-    return true;
+    grpc::ClientContext ctx;
+    CreateTopicRequest request;
+    CreateTopicResponse response;
+    request.set_name(topic);
+    m_stub->CreateTopic(&ctx, request, &response);
+    return response.success();
 }
 
 bool RPCSender::publishMessage(const std::string message)
 {
-
-    return true;
+    grpc::ClientContext ctx;
+    PublishMessageRequest request;
+    PublishMessageResponse response;
+    request.set_message(message);
+    m_stub->PublishMessage(&ctx, request, &response);
+    return response.success();
 }
--- Sender/RPCSender.h
+++ Sender/RPCSender.h
@@ -1,15 +1,25 @@
 #ifndef SENDER_GRPCSENDER_H
 #define SENDER_GRPCSENDER_H
 
+#define BROKER_GRPC_PORT 8081
+#define BROKER_NAME "broker.pad.utm.md"
+
+#include <sstream>
+#include <memory>
 #include "ISender.h"
 #include "Sender.pb.h"
 #include "Sender.grpc.pb.h"
+#include <grpcpp/create_channel.h>
 
 class RPCSender : public ISender
 {
 public:
+    RPCSender();
     bool createTopic(const std::string topic) override;
     bool publishMessage(const std::string message) override;
+
+private:
+    std::unique_ptr<SenderService::Stub> m_stub;
 };
 
 
